65d47749c52c0de56491856ff0b5da5a7c9ab0a8
ibrahim velinov <ibsukru@gmail.com>
2025-11-08 22:07:49 +0100

    🚀

--- apps/web/app/api/auth/[...nextauth]/options.ts
+++ apps/web/app/api/auth/[...nextauth]/options.ts
@@ -190,7 +190,6 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async redirect({ url, baseUrl }) {
-      if (isDevelopment) return process.env.AUTH_URL || "http://localhost:3000"
       // Extract hostname from baseUrl to get site-specific config
       try {
         const baseUrlObj = new URL(baseUrl)
