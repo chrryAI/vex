604f2ac9dd1ed98f54173773541235f9be48d82a
ibrahim velinov <ibsukru@gmail.com>
2025-11-21 15:59:19 +0100

    🚀

--- packages/ui/Focus.tsx
+++ packages/ui/Focus.tsx
@@ -2,12 +2,11 @@
 
 import React from "react"
 import Skeleton from "./Skeleton"
-import styles from "./Focus.module.scss"
 import FocusButton from "./FocusButton"
 
 export default function Focus({ children }: { children?: React.ReactNode }) {
   return (
-    <Skeleton className={styles.page}>
+    <Skeleton>
       <FocusButton />
       {children}
     </Skeleton>
