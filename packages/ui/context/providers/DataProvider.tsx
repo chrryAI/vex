815fa5a92a2a798c34238aa84bcfe6740d66cae2
ibrahim velinov <ibsukru@gmail.com>
2025-11-25 12:24:34 +0100

    🚀

--- packages/ui/context/providers/DataProvider.tsx
+++ packages/ui/context/providers/DataProvider.tsx
@@ -205,7 +205,7 @@ export function DataProvider({ children, ...rest }: { children: ReactNode }) {
   const [loadingAffiliateStats, setLoadingAffiliateStats] =
     useState<boolean>(false)
 
-  const VERSION = "1.4.62"
+  const VERSION = "1.4.63"
 
   const [weather, setWeather] = useLocalStorage<
     | {
