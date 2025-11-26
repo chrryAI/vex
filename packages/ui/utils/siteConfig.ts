29f56e867af93a1d2f46129fe4c60c120b4716c3
ibrahim velinov <ibsukru@gmail.com>
2025-11-26 20:10:36 +0100

    🚀

--- packages/ui/utils/siteConfig.ts
+++ packages/ui/utils/siteConfig.ts
@@ -592,7 +592,7 @@ export function detectSiteModeDomain(
 ): SiteMode {
   const defaultMode =
     (process.env.MODE as SiteMode) ||
-    ((import.meta.env as any)?.VITE_SITE_MODE as SiteMode) ||
+    ((import.meta as any)?.env?.VITE_SITE_MODE as SiteMode) ||
     mode ||
     ("vex" as SiteMode)
 
