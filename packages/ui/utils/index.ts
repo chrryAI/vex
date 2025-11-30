b0d025f31aeda0a91c5b536807ec682250ecff15
ibrahim velinov <ibsukru@gmail.com>
2025-12-01 00:58:56 +0100

    🚀

--- packages/ui/utils/index.ts
+++ packages/ui/utils/index.ts
@@ -375,7 +375,7 @@ export function getFlag({ code }: { code?: string }) {
 
 const config = getSiteConfig(getClientHostname())
 
-export const VERSION = config.version || "1.5.43"
+export const VERSION = config.version || "1.5.44"
 export type instructionBase = {
   id: string
   title: string
--- packages/ui/utils/siteConfig.ts
+++ packages/ui/utils/siteConfig.ts
@@ -175,7 +175,7 @@ const focus = {
   favicon: "focus",
   mode: "focus" as SiteMode,
   slug: "focus",
-  version: "26.10.47",
+  version: "26.10.48",
   storeSlug: "blossom",
   name: "Focus",
   domain: "focus.chrry.ai",
