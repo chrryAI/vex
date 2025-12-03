9b426c72d8721f739f7a95b6936f7f0917f8d7c6
ibrahim velinov <ibsukru@gmail.com>
2025-12-03 13:43:06 +0100

    🚀

--- apps/web/app/[locale]/affiliate/dashboard/page.tsx
+++ apps/web/app/[locale]/affiliate/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import Skeleton from "chrry/Skeleton"
 import React from "react"
-import AffiliateDashboard from "chrry/AffiliateDashboard"
+import AffiliateDashboard from "chrry/affiliateDashboard"
 
 export default function AffiliateDashboardPage() {
   return <AffiliateDashboard />
