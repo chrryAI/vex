dbbf104686e764d0151603cffc0a2ceb6b501ef1
ibrahim velinov <ibsukru@gmail.com>
2025-12-05 18:05:03 +0100

    feat: E2E setup complete with external PR protection

--- packages/ui/context/providers/DataProvider.tsx
+++ packages/ui/context/providers/DataProvider.tsx
@@ -205,7 +205,7 @@ export function DataProvider({ children, ...rest }: { children: ReactNode }) {
   const [loadingAffiliateStats, setLoadingAffiliateStats] =
     useState<boolean>(false)
 
-  const VERSION = "1.5.77"
+  const VERSION = "1.5.78"
 
   const [weather, setWeather] = useLocalStorage<
     | {
