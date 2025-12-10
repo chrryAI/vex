391ecc792a765ac3c42491ed1fdcad2f8f0bd74f
ibrahim velinov <ibsukru@gmail.com>
2025-12-10 11:22:53 +0100

    🚀

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -74,7 +74,7 @@ export default defineConfig(({ command, mode }) => {
   const manifestBase = {
     manifest_version: 3,
     name: `${siteConfig.name} 🍒`,
-    version: siteConfig.version || "1.6.30",
+    version: siteConfig.version || "1.6.31",
     description: siteConfig.description,
     permissions: isFirefox
       ? ["storage", "tabs", "contextMenus", "cookies"] // Firefox doesn't support sidePanel permission
--- packages/ui/utils/index.ts
+++ packages/ui/utils/index.ts
@@ -397,7 +397,7 @@ export function getFlag({ code }: { code?: string }) {
 
 const config = getSiteConfig(getClientHostname())
 
-export const VERSION = config.version || "1.6.30"
+export const VERSION = config.version || "1.6.31"
 export type instructionBase = {
   id: string
   title: string
