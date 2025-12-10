6c06e68d46158530f48a38e695ecaa096fe18734
ibrahim velinov <ibsukru@gmail.com>
2025-12-10 06:54:58 +0100

    🚀

--- packages/ui/utils/siteConfig.ts
+++ packages/ui/utils/siteConfig.ts
@@ -178,7 +178,7 @@ const focus = {
   isStoreApp: false,
   mode: "focus" as SiteMode,
   slug: "focus",
-  version: "26.10.56",
+  version: "26.10.57",
   storeSlug: "blossom",
   name: "Focus",
   domain: "focus.chrry.ai",
