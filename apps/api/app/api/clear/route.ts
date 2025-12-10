8cdd14530cd7f6a613648031c06a7ebec2bf9abb
ibrahim velinov <ibsukru@gmail.com>
2025-12-10 04:43:14 +0100

    🚀

--- apps/api/app/api/clear/route.ts
+++ apps/api/app/api/clear/route.ts
@@ -1,38 +1,34 @@
-import { isDevelopment, isE2E } from "chrry/utils"
+import { isE2E } from "chrry/utils"
 import { NextResponse } from "next/server"
 import cleanupTest from "../../../lib/cleanupTest"
-import { cookies, headers } from "next/headers"
 import getMember from "../../actions/getMember"
 import getGuest from "../../actions/getGuest"
-import { TEST_MEMBER_FINGERPRINTS, TEST_GUEST_FINGERPRINTS } from "@repo/db"
-import { v4 as uuidv4 } from "uuid"
+import { TEST_GUEST_FINGERPRINTS, TEST_MEMBER_EMAILS } from "@repo/db"
 
 export async function POST() {
   if (!isE2E) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
   }
 
-  const member = await getMember({})
+  const member = await getMember()
 
   const guest = await getGuest()
 
-  const fp = member?.fingerprint || guest?.fingerprint
-
-  if (!fp) {
-    return NextResponse.json({ error: "Missing fingerprint" }, { status: 401 })
+  if (!member && !guest) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
   }
 
-  // if (!isDevelopment && member?.role !== "admin") {
-  //   if (member && !TEST_MEMBER_FINGERPRINTS.includes(fp)) {
-  //     return NextResponse.json({ error: "Not a test member" }, { status: 401 })
-  //   }
+  if (member && TEST_MEMBER_EMAILS.includes(member?.email)) {
+    await cleanupTest()
 
-  //   if (guest && !TEST_GUEST_FINGERPRINTS.includes(fp)) {
-  //     return NextResponse.json({ error: "Not a test guest" }, { status: 401 })
-  //   }
-  // }
+    return NextResponse.json({ success: true })
+  }
 
-  await cleanupTest()
+  if (guest && TEST_GUEST_FINGERPRINTS.includes(guest?.fingerprint)) {
+    await cleanupTest()
 
-  return NextResponse.json({ success: true })
+    return NextResponse.json({ success: true })
+  }
+
+  return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
 }
