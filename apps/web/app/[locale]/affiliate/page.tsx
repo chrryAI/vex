193c3b07c4897fff76ab8172aada75911873e8dd
ibrahim velinov <ibsukru@gmail.com>
2025-12-14 16:42:07 +0100

    🚀

--- apps/web/app/[locale]/affiliate/page.tsx
+++
@@ -1,7 +0,0 @@
-import Skeleton from "chrry/Skeleton"
-import React from "react"
-import Affiliate from "chrry/affiliate"
-
-export default function AffiliatePage() {
-  return <Affiliate />
-}
--- apps/web/app/[locale]/calendar/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Calendar from "chrry/Calendar"
-
-export default function page() {
-  return <Calendar defaultView="month" defaultDate={new Date()} />
-}
