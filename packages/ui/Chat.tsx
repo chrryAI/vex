9b79d4baf7b38b02c563e28f48f7196c1030295b
ibrahim velinov <ibsukru@gmail.com>
2025-12-23 14:40:29 +0100

    🚀

--- packages/ui/Chat.tsx
+++ packages/ui/Chat.tsx
@@ -3257,7 +3257,7 @@ export default function Chat({
           ...style,
           ...(isDrawerOpen && !isSmallDevice ? styles.drawerOpen.style : {}),
           // ...(isMobileDevice ? styles.mobile.style : {}),
-          ...(isStandalone && os === "ios" ? { marginBottom: 14.5 } : {}),
+          ...(isStandalone && os === "ios" ? { marginBottom: 17.5 } : {}),
         }}
       >
         {isSpeechActive && (
--- packages/ui/context/providers/DataProvider.tsx
+++ packages/ui/context/providers/DataProvider.tsx
@@ -200,7 +200,7 @@ export function DataProvider({ children, ...rest }: { children: ReactNode }) {
   const [loadingAffiliateStats, setLoadingAffiliateStats] =
     useState<boolean>(false)
 
-  const VERSION = "1.7.90"
+  const VERSION = "1.7.91"
 
   const [weather, setWeather] = useLocalStorage<
     | {
