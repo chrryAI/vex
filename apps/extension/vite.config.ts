652dec297ffda3b983629abb04daa29113265bd6
ibrahim velinov <ibsukru@gmail.com>
2025-12-23 23:10:23 +0100

    🚀

--- apps/extension/vite.config.ts
+++ apps/extension/vite.config.ts
@@ -78,7 +78,7 @@ export default async ({ command, mode }) => {
   const manifestBase = {
     manifest_version: 3,
     name: `${siteConfig.name} 🍒`,
-    version: siteConfig.version || "1.7.99",
+    version: siteConfig.version || "1.8.0",
     description: siteConfig.description,
     permissions: isFirefox
       ? ["storage", "tabs", "contextMenus", "cookies"] // Firefox doesn't support sidePanel permission
