a275be4f0947f28577e59fa20e24cd903899f055
ibrahim velinov <ibsukru@gmail.com>
2025-12-24 16:48:42 +0100

    🚀

--- apps/sushi/src/types/firefox.d.ts
+++
@@ -1,9 +0,0 @@
-interface FirefoxBrowser {
-  sidebarAction: {
-    open(): Promise<void>
-    close(): Promise<void>
-    isOpen(): Promise<boolean>
-  }
-}
-
-declare const browser: FirefoxBrowser
