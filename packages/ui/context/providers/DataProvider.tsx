d3b227b65842c5ea835f1a73b7fea2fd01ff8271
ibrahim velinov <ibsukru@gmail.com>
2025-12-29 06:39:14 +0100

    🚀

--- packages/ui/context/providers/DataProvider.tsx
+++ packages/ui/context/providers/DataProvider.tsx
@@ -200,7 +200,7 @@ export function DataProvider({ children, ...rest }: { children: ReactNode }) {
   const [loadingAffiliateStats, setLoadingAffiliateStats] =
     useState<boolean>(false)
 
-  const VERSION = "1.8.70"
+  const VERSION = "1.8.71"
 
   const [weather, setWeather] = useLocalStorage<
     | {
--- packages/ui/utils/index.ts
+++ packages/ui/utils/index.ts
@@ -394,7 +394,7 @@ export function getFlag({ code }: { code?: string }) {
 
 const config = getSiteConfig(getClientHostname())
 
-export const VERSION = config.version || "1.8.70"
+export const VERSION = config.version || "1.8.71"
 export type instructionBase = {
   id: string
   title: string
