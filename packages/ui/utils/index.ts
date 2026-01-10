b1dc76ca2870aae7c4205049ba4622707d980cde
ibrahim velinov <ibsukru@gmail.com>
2026-01-10 13:21:59 +0100

    🚀

--- packages/ui/utils/index.ts
+++ packages/ui/utils/index.ts
@@ -394,7 +394,7 @@ export function getFlag({ code }: { code?: string }) {
 
 const config = getSiteConfig(getClientHostname())
 
-export const VERSION = config.version || "1.10.6"
+export const VERSION = config.version || "1.10.7"
 export type instructionBase = {
   id: string
   title: string
