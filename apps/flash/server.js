e075076a32b0960cbabef1b43fa5bc96dd3f73b2
ibrahim velinov <ibsukru@gmail.com>
2026-03-13 23:21:06 +0100

    🚀

--- apps/flash/server.js
+++ apps/flash/server.js
@@ -695,7 +695,7 @@ export const whiteLabels = [
   _tribe,
 ]
 
-const VERSION = "2.0.70"
+const VERSION = "2.0.71"
 // Constants
 const port = process.env.PORT || 5173
 const base = process.env.BASE || "/"
