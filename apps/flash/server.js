43c257b90f413cd0efaf34e664bd6169242186f7
ibrahim velinov <ibsukru@gmail.com>
2026-03-22 11:13:40 +0100

    🚀

--- apps/flash/server.js
+++ apps/flash/server.js
@@ -710,7 +710,7 @@ export const whiteLabels = [
   _tribe,
 ]
 
-const VERSION = "2.1.65"
+const VERSION = "2.1.66"
 // Constants
 const port = process.env.PORT || 5173
 const base = process.env.BASE || "/"
--- packages/ui/context/providers/AuthProvider.tsx
+++ packages/ui/context/providers/AuthProvider.tsx
@@ -98,7 +98,7 @@ import { useError } from "./ErrorProvider"
 
 export type { session }
 
-const VERSION = "2.1.65"
+const VERSION = "2.1.66"
 
 const AuthContext = createContext<
   | {
