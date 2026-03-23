d5d3e506df1c0ef90279533fe681741b675e0c13
ibrahim velinov <ibsukru@gmail.com>
2026-03-23 01:13:49 +0100

    🚀

--- packages/ui/Watermelon.tsx
+++ packages/ui/Watermelon.tsx
@@ -1146,7 +1146,13 @@ export default function Watermelon() {
                       </A>
                       <Button
                         className="inverted"
-                        onClick={() => setShowS3(true)}
+                        onClick={() => {
+                          if (!user && !s3ApiKey) {
+                            addParams({ subscribe: "true", plan: "member" })
+                            return
+                          }
+                          setShowS3(true)
+                        }}
                         style={{
                           ...utilities.inverted.style,
                           display: "flex",
