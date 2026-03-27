07763bdb187019748fa72c4467b08ea86b04af5b
ibrahim velinov <ibsukru@gmail.com>
2026-03-27 23:18:06 +0100

    🚀

--- packages/ui/context/providers/AuthProvider.tsx
+++ packages/ui/context/providers/AuthProvider.tsx
@@ -104,7 +104,7 @@ export type { session }
 // Create a dedicated low-priority queue for analytics so it doesn't block SWR data fetching
 const analyticsLimit = pLimit(1)
 
-const VERSION = "2.2.69"
+const VERSION = "2.2.70"
 
 const AuthContext = createContext<
   | {
